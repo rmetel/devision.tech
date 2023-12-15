5d10e4e817f3baa47402ac77300a699b841a49b5
ralph.metel@gmx.de <pOison_2(>
2023-12-15 20:08:41 +0100

    added logo

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,23 +6,49 @@ export const NavBar: React.FC = () => {
       <div className="container">
         {/* Brand and toggle get grouped for better mobile display */}
         <div className="navbar-header">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
-                  data-target="#nav-icon-collapse" aria-expanded="false">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#nav-icon-collapse"
+            aria-expanded="false"
+          >
             <span className="sr-only">Toggle navigation</span>
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
-          <a className="logo" href="#">Metzner</a>
+
+          <img src="/img/logo.png" className="logo-fullscreen" />
+
+          <a className="logo" href="#">
+            Metzner
+          </a>
         </div>
 
         {/* Collect the nav links, and other content for toggling */}
         <div className="collapse navbar-collapse" id="nav-icon-collapse">
           <ul className="nav navbar-nav navbar-right">
-            <li><a href="#" data-scroll-nav="0" className="active">Home</a></li>
-            <li><a href="#" data-scroll-nav="1">Leistungen</a></li>
-            <li><a href="#" data-scroll-nav="2">Portfolio</a></li>
-            <li><a href="#" data-scroll-nav="3">Kontakt</a></li>
+            <li>
+              <a href="#" data-scroll-nav="0" className="active">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" data-scroll-nav="1">
+                Leistungen
+              </a>
+            </li>
+            <li>
+              <a href="#" data-scroll-nav="2">
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a href="#" data-scroll-nav="3">
+                Kontakt
+              </a>
+            </li>
           </ul>
         </div>
       </div>
