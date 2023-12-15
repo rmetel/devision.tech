8fcb8ad797d28ba09d74c795e6b593c2e2d43669
ralph.metel@gmx.de <pOison_2(>
2023-12-15 20:59:04 +0100

    added portfolio images

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,159 +1,53 @@
 import React from "react";
 
 export const Portfolio: React.FC = () => {
+  const images = [
+    "/img/portfolio/portfolio-1.jpg",
+    "/img/portfolio/portfolio-1.jpg",
+    "/img/portfolio/portfolio-1.jpg",
+    "/img/portfolio/portfolio-1.jpg",
+    "/img/portfolio/portfolio-1.jpg",
+    "/img/portfolio/portfolio-1.jpg",
+  ];
+
   return (
-    <section className="portfolio section-padding pb-70 bg-gray" data-scroll-index="2">
+    <section
+      className="portfolio section-padding pb-70 bg-gray"
+      data-scroll-index="2"
+    >
       <div className="container">
         <div className="row">
-
           <div className="section-head">
             <h3>Portfolio.</h3>
           </div>
 
           <div className="gallery text-center">
-            <div className="col-md-4 col-sm-6 items graphic">
-              <div className="item-img">
-                <img src="/img/portfolio/placeholder.jpg" alt="image" />
-                <div className="item-img-overlay">
-                  <div className="overlay-info v-middle text-center">
-                    <h6 className="sm-titl">WEB DESIGN</h6>
-                    <div className="icons">
-											<span className="icon">
-												<a href="#0">
-													<i className="fa fa-chain-broken" aria-hidden="true"></i>
-												</a>
-											</span>
-
-                      <span className="icon link">
-												<a href="/img/portfolio/placeholder.jpg">
-													<i className="fa fa-search-plus" aria-hidden="true"></i>
-												</a>
-											</span>
+            {images.map((image) => {
+              return (
+                <>
+                  <div className="col-md-4 col-sm-6 items graphic">
+                    <div className="item-img">
+                      <img src={image} alt="image" />
+                      <div className="item-img-overlay">
+                        <div className="overlay-info v-middle text-center">
+                          <h6 className="sm-titl">SOLAR-PANEL</h6>
+                          <div className="icons">
+                            <span className="icon link">
+                              <a href={image}>
+                                <i
+                                  className="fa fa-search-plus"
+                                  aria-hidden="true"
+                                ></i>
+                              </a>
+                            </span>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-4 col-sm-6 items web">
-              <div className="item-img">
-                <img src="/img/portfolio/placeholder.jpg" alt="image" />
-                <div className="item-img-overlay">
-                  <div className="overlay-info v-middle text-center">
-                    <h6 className="sm-titl">WEB DESIGN</h6>
-                    <div className="icons">
-											<span className="icon">
-												<a href="#0">
-													<i className="fa fa-chain-broken" aria-hidden="true"></i>
-												</a>
-											</span>
-
-                      <span className="icon link">
-												<a href="/img/portfolio/placeholder.jpg">
-													<i className="fa fa-search-plus" aria-hidden="true"></i>
-												</a>
-											</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-4 col-sm-6 items graphic">
-              <div className="item-img">
-                <img src="/img/portfolio/placeholder.jpg" alt="image" />
-                <div className="item-img-overlay">
-                  <div className="overlay-info v-middle text-center">
-                    <h6 className="sm-titl">WEB DESIGN</h6>
-                    <div className="icons">
-											<span className="icon">
-												<a href="#0">
-													<i className="fa fa-chain-broken" aria-hidden="true"></i>
-												</a>
-											</span>
-
-                      <span className="icon link">
-												<a href="/img/portfolio/placeholder.jpg">
-													<i className="fa fa-search-plus" aria-hidden="true"></i>
-												</a>
-											</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-4 col-sm-6 items brand">
-              <div className="item-img">
-                <img src="/img/portfolio/placeholder.jpg" alt="image" />
-                <div className="item-img-overlay">
-                  <div className="overlay-info v-middle text-center">
-                    <h6 className="sm-titl">WEB DESIGN</h6>
-                    <div className="icons">
-											<span className="icon">
-												<a href="#0">
-													<i className="fa fa-chain-broken" aria-hidden="true"></i>
-												</a>
-											</span>
-
-                      <span className="icon link">
-												<a href="/img/portfolio/placeholder.jpg">
-													<i className="fa fa-search-plus" aria-hidden="true"></i>
-												</a>
-											</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-4 col-sm-6 items web graphic">
-              <div className="item-img">
-                <img src="/img/portfolio/placeholder.jpg" alt="image" />
-                <div className="item-img-overlay">
-                  <div className="overlay-info v-middle text-center">
-                    <h6 className="sm-titl">WEB DESIGN</h6>
-                    <div className="icons">
-											<span className="icon">
-												<a href="#0">
-													<i className="fa fa-chain-broken" aria-hidden="true"></i>
-												</a>
-											</span>
-
-                      <span className="icon link">
-												<a href="/img/portfolio/placeholder.jpg">
-													<i className="fa fa-search-plus" aria-hidden="true"></i>
-												</a>
-											</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-4 col-sm-6 items brand">
-              <div className="item-img">
-                <img src="/img/portfolio/placeholder.jpg" alt="image" />
-                <div className="item-img-overlay">
-                  <div className="overlay-info v-middle text-center">
-                    <h6 className="sm-titl">WEB DESIGN</h6>
-                    <div className="icons">
-											<span className="icon">
-												<a href="#0">
-													<i className="fa fa-chain-broken" aria-hidden="true"></i>
-												</a>
-											</span>
-
-                      <span className="icon link">
-												<a href="/img/portfolio/placeholder.jpg">
-													<i className="fa fa-search-plus" aria-hidden="true"></i>
-												</a>
-											</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+                </>
+              );
+            })}
           </div>
         </div>
       </div>
